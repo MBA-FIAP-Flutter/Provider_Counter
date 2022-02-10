180b350189327b3e5df53bc3bbdec4eca0279a67
Ricardo da Silva Ogliari <ricardo@omni.ai>
2022-02-10 00:09:58 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:counter_provider/counter_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterModel(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var counterModel = Provider.of<CounterModel>(context, listen: false);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Provider Example"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.clear_all),
+            onPressed: (){
+              counterModel.reset();
+            },
+          )
+        ]
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Consumer<CounterModel>(
+              builder: (context, cart, child) {
+                return Text(
+                  '${cart.counter}',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          counterModel.increment();
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
